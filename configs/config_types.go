2fa0d45346e480003d8f2832555ee31ea10e035c
Sundaram Kumar Jha <jhasundaram@outlook.com>
2024-11-27 19:46:30 +0530

    refactor: updated the helper function to structure of smurf yaml file

--- configs/config_types.go
+++ configs/config_types.go
@@ -3,19 +3,27 @@ package configs
 var FileName = "smurf.yaml"
 
 type Config struct {
-	ProvisionAcrSubscriptionID   string `yaml:"provisionAcrSubscriptionID"`
-	ProvisionAcrResourceGroup    string `yaml:"provisionAcrResourceGroup"`
+	Sdkr SdkrConfig `yaml:"sdkr"`
+	Selm SelmConfig `yaml:"selm"`
+}
+
+type SdkrConfig struct {
+	DockerPassword               string `yaml:"docker_password"`
+	DockerUsername               string `yaml:"docker_username"`
 	ProvisionAcrRegistryName     string `yaml:"provisionAcrRegistryName"`
+	ProvisionAcrResourceGroup    string `yaml:"provisionAcrResourceGroup"`
+	ProvisionAcrSubscriptionID   string `yaml:"provisionAcrSubscriptionID"`
 	ProvisionEcrRegion           string `yaml:"provisionEcrRegion"`
 	ProvisionEcrRepository       string `yaml:"provisionEcrRepository"`
 	ProvisionGcrProjectID        string `yaml:"provisionGcrProjectID"`
-	DockerUsername               string `yaml:"docker_username"`
-	DockerPassword               string `yaml:"docker_password"`
+	GoogleApplicationCredentials string `yaml:"google_application_credentials"`
 	SourceTag                    string `yaml:"sourceTag"`
 	TargetTag                    string `yaml:"targetTag"`
-	Namespace                    string `yaml:"namespace"`
-	Revision                     int    `yaml:"revision"`
-	ChartName                    string `yaml:"chartName"`
-	ChartDir                     string `yaml:"chartDir"`
-	GoogleApplicationCredentials string `yaml:"google_application_credentials"`
+}
+
+type SelmConfig struct {
+	ReleaseName string `yaml:"releaseName"`
+	Namespace   string `yaml:"namespace"`
+	ChartName   string `yaml:"chartName"`
+	Revision    int    `yaml: "revision"`
 }
