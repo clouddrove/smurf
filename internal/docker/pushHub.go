74338d88ae167f1b57a05bc0f5049031017fcbf5
Sundaram Kumar Jha <jhasundaram@outlook.com>
2024-12-24 05:00:18 +0530

    feat: push fn  for docker hub

---
+++ internal/docker/pushHub.go
@@ -0,0 +1,49 @@
+package docker
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/docker/docker/api/types/image"
+	"github.com/docker/docker/api/types/registry"
+	"github.com/docker/docker/client"
+	"github.com/pterm/pterm"
+)
+
+// PushImage pushes a Docker image to a Docker registry.
+func PushImage(opts PushOptions) error {
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		pterm.Error.Println("Error creating Docker client:", err)
+		return err
+	}
+
+	authConfig := registry.AuthConfig{
+		Username: os.Getenv("DOCKER_USERNAME"),
+		Password: os.Getenv("DOCKER_PASSWORD"),
+	}
+	encodedJSON, err := json.Marshal(authConfig)
+	if err != nil {
+		pterm.Error.Println("Error encoding auth config:", err)
+		return err
+	}
+	authStr := base64.URLEncoding.EncodeToString(encodedJSON)
+
+	spinner, _ := pterm.DefaultSpinner.Start(fmt.Sprintf("Pushing image %s...", opts.ImageName))
+	options := image.PushOptions{
+		RegistryAuth: authStr,
+	}
+
+	responseBody, err := cli.ImagePush(ctx, opts.ImageName, options)
+	if err != nil {
+		spinner.Fail("Failed to push the image: " + err.Error())
+		return err
+	}
+	defer responseBody.Close()
+
+	return handleDockerResponse(responseBody, spinner, opts)
+}
