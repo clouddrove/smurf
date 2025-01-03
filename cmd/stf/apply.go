9c1d652bba8f373ca9dcc10256b9150c4ba19922
Sundaram Kumar Jha <jhasundaram@outlook.com>
2025-01-04 02:06:26 +0530

    refactor: added approve flags, default to yes

--- cmd/stf/apply.go
+++ cmd/stf/apply.go
@@ -1,6 +1,7 @@
 package stf
 
 import (
+	"github.com/clouddrove/smurf/configs"
 	"github.com/clouddrove/smurf/internal/terraform"
 	"github.com/spf13/cobra"
 )
@@ -10,8 +11,8 @@ var applyCmd = &cobra.Command{
 	Use:   "apply",
 	Short: "Apply the changes required to reach the desired state of Terraform Infrastructure",
 	RunE: func(cmd *cobra.Command, args []string) error {
-
-		return terraform.Apply()
+		approve := configs.CanApply
+		return terraform.Apply(approve)
 	},
 	Example: `
 	smurf stf apply
@@ -19,5 +20,6 @@ var applyCmd = &cobra.Command{
 }
 
 func init() {
+	stfCmd.Flags().BoolVar(&configs.CanApply, "approve", true, "Skip interactive approval of plan before applying")
 	stfCmd.AddCommand(applyCmd)
 }
