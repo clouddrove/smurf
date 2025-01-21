86d69960f404016dd9a906e7b775b1474cc8233f
Sundaram Kumar Jha <jhasundaram@outlook.com>
2025-01-22 00:19:26 +0530

    refactor: added support for varfiles, variables and lock values

--- internal/terraform/apply.go
+++ internal/terraform/apply.go
@@ -12,11 +12,12 @@ import (
 	"github.com/pterm/pterm"
 )
 
+
 // Apply executes 'apply' to apply the planned changes.
 // It initializes the Terraform client, runs the apply operation with a spinner for user feedback,
 // and handles any errors that occur during the process. Upon successful completion,
-// it sets custom writers for stdout and stderr to handle colored output.
-func Apply(approve bool) error {
+// it sets custom writers for stdout and stderr to handle colored output. lock is by default false
+func Apply(approve bool, vars []string, varFiles []string, lock bool) error {
 	tf, err := GetTerraform()
 	if err != nil {
 		return err
@@ -27,7 +28,27 @@ func Apply(approve bool) error {
 		Printf("Terraform Apply")
 	fmt.Println()
 
-	_, err = tf.Plan(context.Background(), tfexec.Out("plan.out"))
+	applyOptions := []tfexec.PlanOption{
+		tfexec.Out("plan.out"),
+	}
+
+	if vars != nil {
+		pterm.Info.Printf("Setting variable: %s\n", vars)
+		for _, v := range vars {
+			pterm.Info.Printf("Setting variable: %s\n", v)
+			applyOptions = append(applyOptions, tfexec.Var(v))
+		}
+	}
+
+	if varFiles != nil {
+		pterm.Info.Printf("Setting variable file: %s\n", varFiles)
+		for _, vf := range varFiles {
+			pterm.Info.Printf("Loading variable file: %s\n", vf)
+			applyOptions = append(applyOptions, tfexec.VarFile(vf))
+		}
+	}
+
+	_, err = tf.Plan(context.Background(), applyOptions...)
 	if err != nil {
 		pterm.Error.Printf("Failed to generate plan: %v\n", err)
 		return err
@@ -76,7 +97,12 @@ func Apply(approve bool) error {
 	tf.SetStdout(os.Stdout)
 	tf.SetStderr(os.Stderr)
 
-	err = tf.Apply(context.Background(), tfexec.DirOrPlan("plan.out"))
+	Options := []tfexec.ApplyOption{
+		tfexec.Lock(lock),
+		tfexec.DirOrPlan("plan.out"),
+	}
+
+	err = tf.Apply(context.Background(), Options...)
 	if err != nil {
 		spinner.Fail("Apply failed")
 		pterm.Error.Printf("Error: %v\n", err)
