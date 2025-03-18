9a6fe9c69b0114b4dbd95c6cfc6e45d22c34d1a7
Bharadwajshivam28 <shivambharadwaj822@gmail.com>
2025-03-18 20:29:53 +0530

    feat: Added --auto-approve flag

--- cmd/stf/apply.go
+++ cmd/stf/apply.go
@@ -10,19 +10,25 @@ var applyVarNameValue []string
 var applyVarFile []string
 var applyLock bool
 var applyDir string
+var applyAutoApprove bool
 
+// applyCmd defines a subcommand that applies the changes required to reach the desired state of Terraform Infrastructure.
 // applyCmd defines a subcommand that applies the changes required to reach the desired state of Terraform Infrastructure.
 var applyCmd = &cobra.Command{
 	Use:   "apply",
 	Short: "Apply the changes required to reach the desired state of Terraform Infrastructure",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return terraform.Apply(applyApprove, applyVarNameValue, applyVarFile, applyLock, applyDir)
+		approve := applyApprove || applyAutoApprove
+		return terraform.Apply(approve, applyVarNameValue, applyVarFile, applyLock, applyDir)
 	},
 	Example: `
 	smurf stf apply
 
 	# Specify variables
 	smurf stf apply -var="region=us-west-2"
+
+	# Skip approval prompt
+	smurf stf apply --auto-approve
 
 	# Specify multiple variables
 	smurf stf apply -var="region=us-west-2" -var="instance_type=t2.micro"
@@ -38,6 +44,7 @@ func init() {
 	applyCmd.Flags().BoolVar(&applyApprove, "approve", false, "Skip interactive approval of plan before applying")
 	applyCmd.Flags().BoolVar(&applyLock, "lock", true, "Don't hold a state lock during the operation. This is dangerous if others might concurrently run commands against the same workspace.")
 	applyCmd.Flags().StringVar(&applyDir, "dir", ".", "Specify the directory containing Terraform files")
+	applyCmd.Flags().BoolVar(&applyAutoApprove, "auto-approve", false, "Skip interactive approval of plan before applying")
 
 	stfCmd.AddCommand(applyCmd)
 }
