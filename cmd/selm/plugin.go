7f509357ccd6a3d99cac094b7bfc6e669aa5dfa0
Bharadwajshivam28 <shivambharadwaj822@gmail.com>
2025-03-24 20:58:03 +0530

    integrated multiple plugin install feature

--- cmd/selm/plugin.go
+++ cmd/selm/plugin.go
@@ -9,29 +9,30 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var pluginName string
+var pluginNames string
 
-// pluginInstallCmd is a subcommand that installs a Helm plugin.
+// pluginInstallCmd is a subcommand that installs multiple Helm plugins.
 var pluginInstallCmd = &cobra.Command{
-	Use:   "plugin install [PLUGIN]",
-	Short: "Install a Helm plugin.",
+	Use:   "plugin install [PLUGINS]",
+	Short: "Install one or more Helm plugins (comma-separated).",
 	Args:  cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		pluginName = args[0]
-		if pluginName == "" {
-			return errors.New(color.RedString("PLUGIN name must be provided"))
+		pluginNames = args[0]
+		if pluginNames == "" {
+			return errors.New(color.RedString("At least one PLUGIN name must be provided"))
 		}
 
-		pterm.Info.Println(fmt.Sprintf("Installing Helm plugin: %s", pluginName))
-		err := helm.HelmPluginInstall(pluginName)
+		pterm.Info.Println(fmt.Sprintf("Installing Helm plugins: %s", pluginNames))
+		err := helm.HelmPluginInstall(pluginNames)
 		if err != nil {
 			return errors.New(color.RedString("Helm plugin install failed: %v", err))
 		}
-		pterm.Success.Println("Helm plugin installed successfully.")
+		pterm.Success.Println("Helm plugins installed successfully.")
 		return nil
 	},
 	Example: `
   smurf selm plugin https://github.com/helm/helm-secrets
+  smurf selm plugin https://github.com/helm/helm-secrets,https://github.com/helm/helm-diff
   `,
 }
 
