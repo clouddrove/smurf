b84d16e36a6d46d90537f72ad706a36692a0c975
Anket7 <anket.plainsurf@gmail.com>
2025-04-30 22:06:37 +0530

    add --version flag to ckeck the version of smurt CLI tool (#138)
    * add --version flag to ckeck the version of smurt CLI tool
    
    * resolve the issue of function  override
    
    * Remove GitHub API dependency
    
    * Added version capture from git tag in CI
    
    * resolve issue of check formatting job
    
    ---------
    
    Co-authored-by: Anket <anket.satbhai@plainsurf.com>

--- cmd/version.go
+++ cmd/version.go
@@ -1,73 +1,42 @@
 package cmd
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
+	"os"
 
 	"github.com/spf13/cobra"
 )
 
-var (
-	version = "v1.0.0"
-)
+// version will be set during build via ldflags
+var version = "v1.0.0"
 
-type githubRelease struct {
-	TagName string `json:"tag_name"`
-}
-
-func getLatestVersion() string {
-	url := "https://api.github.com/repos/clouddrove/smurf/releases/latest"
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		fmt.Println("Error creating request:", err)
-		return version
-	}
-	req.Header.Add("Accept", "application/vnd.github.v3+json")
-
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Error sending request:", err)
-		return version
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		fmt.Println("Error: Unable to fetch latest version. Status code:", resp.StatusCode)
-		return version
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error reading response:", err)
-		return version
-	}
-
-	var release githubRelease
-	err = json.Unmarshal(body, &release)
-	if err != nil {
-		fmt.Println("Error parsing JSON:", err)
-		return version
-	}
-
-	if release.TagName != "" {
-		return release.TagName
-	}
-
-	return version
-}
-
-// versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Show the version of your CLI tool",
+	Short: "Print the version number",
 	Run: func(cmd *cobra.Command, args []string) {
-		latestVersion := getLatestVersion()
-		fmt.Printf("%s\n", latestVersion)
+		fmt.Println(version)
 	},
 }
 
 func init() {
+	// Version flag  --version
+	RootCmd.Flags().BoolP("version", "", false, "Show version information")
+
+	// Preserve existing PersistentPreRun if it exists
+	originalPersistentPreRun := RootCmd.PersistentPreRun
+
+	RootCmd.PersistentPreRun = func(cmd *cobra.Command, args []string) {
+		// Handle --version flag first
+		if v, _ := cmd.Flags().GetBool("version"); v {
+			fmt.Println(version)
+			os.Exit(0)
+		}
+
+		// Chain to original PersistentPreRun if exists
+		if originalPersistentPreRun != nil {
+			originalPersistentPreRun(cmd, args)
+		}
+	}
+
 	RootCmd.AddCommand(versionCmd)
 }
