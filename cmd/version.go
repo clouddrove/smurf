f3fde4ecf2b4349755908502ddeeeb45311e8b33
Anket Satbhai <anket-satbhai@anket-satbhai.lan>
2025-05-15 14:30:22 +0530

    update smurf version and --version flag

--- cmd/version.go
+++ cmd/version.go
@@ -2,41 +2,21 @@ package cmd
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/spf13/cobra"
 )
 
-// version will be set during build via ldflags
-var version = "v1.0.0"
-
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Print the version number",
+	Short: "Print detailed version information",
+	Long:  `Print the version number of Smurf CLI along with build information`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(version)
+		printVersion()
 	},
 }
 
-func init() {
-	// Version flag  --version
-	RootCmd.Flags().BoolP("version", "", false, "Show version information")
-
-	// Preserve existing PersistentPreRun if it exists
-	originalPersistentPreRun := RootCmd.PersistentPreRun
-
-	RootCmd.PersistentPreRun = func(cmd *cobra.Command, args []string) {
-		// Handle --version flag first
-		if v, _ := cmd.Flags().GetBool("version"); v {
-			fmt.Println(version)
-			os.Exit(0)
-		}
-
-		// Chain to original PersistentPreRun if exists
-		if originalPersistentPreRun != nil {
-			originalPersistentPreRun(cmd, args)
-		}
-	}
-
-	RootCmd.AddCommand(versionCmd)
+func printVersion() {
+	fmt.Printf("Smurf CLI version: %s\n", version)
+	fmt.Printf("Git commit: %s\n", commit)
+	fmt.Printf("Build date: %s\n", date)
 }
